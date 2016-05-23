10f10713c3e133ae5da7160837f7e5abbdd2d2b4
Hugo Aguirre <bul.ikana@gmail.com>
2016-05-23 12:57:20 -0500

    Loading JSON from remote URL

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,38 @@
 angular.module('starter.controllers', ['starter.services'])
 
-.controller('FileController', function($scope, $cordovaFile, $state, DataService) {
+.controller('FileController', function($scope, $cordovaFile, $state, DataService, $http) {
 	$scope.file = {};
 
 	$scope.file.spinner = false;
 
-	$scope.file.openFromLocal = function () {
+	$scope.file.openFromLocal = function() {
 		$scope.file.spinner = true;
 
 		$cordovaFile.readAsText(cordova.file.applicationDirectory, "www/data/cats.json")
-      		.then(function (success) {
-      			DataService.data = JSON.parse(success);
-      			DataService.source = "www";
+			.then(function (success) {
+				DataService.data = JSON.parse(success);
+				DataService.source = "www";
 				$scope.file.spinner = false;
-	        	$state.go("list");
-      	}, function (error) {
-      		$scope.file.spinner = false;
-        	console.log(error)
-      });
+				$state.go("list");
+			}, function (error) {
+				$scope.file.spinner = false;
+				console.log(error)
+			});
+	}
+
+	$scope.file.openFromRemote = function() {
+		$scope.file.spinner = true;
+
+		$http.get('http://hugo.mecabotware.com/files/loadcats.json')
+			.then(function (success) {
+				DataService.data = success.data;
+				DataService.source = "remote";
+				$scope.file.spinner = false;
+				$state.go("list");
+			}, function (error) {
+				$scope.file.spinner = false;
+				console.log(error)
+			});
 	}
 })
 
